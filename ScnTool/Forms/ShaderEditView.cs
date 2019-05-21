439f3e9200a5dea98c6d747e9b64ad9aea911170
aizuon <tokay.dorukhan@hotmail.com>
2019-05-21 22:38:29 +0300

    check none if no shader is selected

--- ScnTool/Forms/ShaderEditView.cs
+++ ScnTool/Forms/ShaderEditView.cs
@@ -78,90 +78,136 @@ namespace NetsphereScnTool.Forms
                 foreach (var c in cbx)
                     c.Checked = false;
             }
+            else
+                CheckIfNoShaderChecked();
         }
 
         private void Transparent_CheckedChanged(object sender, EventArgs e)
         {
             if (Transparent.Checked == true)
                 None.Checked = false;
+            else
+                CheckIfNoShaderChecked();
         }
 
         private void NoLight_CheckedChanged(object sender, EventArgs e)
         {
             if (NoLight.Checked == true)
                 None.Checked = false;
+            else
+                CheckIfNoShaderChecked();
         }
 
         private void Cutout_CheckedChanged(object sender, EventArgs e)
         {
             if (Cutout.Checked == true)
                 None.Checked = false;
+            else
+                CheckIfNoShaderChecked();
         }
 
         private void Billboard_CheckedChanged(object sender, EventArgs e)
         {
             if (Billboard.Checked == true)
                 None.Checked = false;
+            else
+                CheckIfNoShaderChecked();
         }
 
         private void NoCulling_CheckedChanged(object sender, EventArgs e)
         {
             if (NoCulling.Checked == true)
                 None.Checked = false;
+            else
+                CheckIfNoShaderChecked();
         }
 
         private void Flare_CheckedChanged(object sender, EventArgs e)
         {
             if (Flare.Checked == true)
                 None.Checked = false;
+            else
+                CheckIfNoShaderChecked();
         }
 
         private void ZWriteOff_CheckedChanged(object sender, EventArgs e)
         {
             if (ZWriteOff.Checked == true)
                 None.Checked = false;
+            else
+                CheckIfNoShaderChecked();
         }
 
         private void Shader_CheckedChanged(object sender, EventArgs e)
         {
             if (Shader.Checked == true)
                 None.Checked = false;
+            else
+                CheckIfNoShaderChecked();
         }
 
         private void NoFog_CheckedChanged(object sender, EventArgs e)
         {
             if (NoFog.Checked == true)
                 None.Checked = false;
+            else
+                CheckIfNoShaderChecked();
         }
 
         private void NoMipmap_CheckedChanged(object sender, EventArgs e)
         {
             if (NoMipmap.Checked == true)
                 None.Checked = false;
+            else
+                CheckIfNoShaderChecked();
         }
 
         private void Shadow_CheckedChanged(object sender, EventArgs e)
         {
             if (Shadow.Checked == true)
                 None.Checked = false;
+            else
+                CheckIfNoShaderChecked();
         }
 
         private void Water_CheckedChanged(object sender, EventArgs e)
         {
             if (Water.Checked == true)
                 None.Checked = false;
+            else
+                CheckIfNoShaderChecked();
         }
 
         private void Distortion_CheckedChanged(object sender, EventArgs e)
         {
             if (Distortion.Checked == true)
                 None.Checked = false;
+            else
+                CheckIfNoShaderChecked();
         }
 
         private void Dark_CheckedChanged(object sender, EventArgs e)
         {
             if (Dark.Checked == true)
                 None.Checked = false;
+            else
+                CheckIfNoShaderChecked();
+        }
+
+        private void CheckIfNoShaderChecked()
+        {
+            uint i = 0;
+
+            foreach (var c in cbx)
+            {
+                if (c.Checked == true)
+                {
+                    i++;
+                }
+            }
+
+            if (i == 0)
+                None.Checked = true;
         }
     }
 }
